726565292dc93f93444d96db6f3eeef551412456
Hossein Ghadirzadeh <hosseinghadirzadeh81@gmail.com>
2021-07-12 05:01:25 +0430

    ADD double Bullets.

--- View.cpp
+++ View.cpp
@@ -115,7 +115,7 @@ void View::keyPressEvent(QKeyEvent* click)
         bullet=new Bullet();
         scene->addItem(bullet);
         bullet->setPos(spaceship->x()+36,spaceship->y()-36);
-            if(false){//TODO****************************gift
+            if(true){//TODO****************************gift
                 bullet2=new Bullet();
                 scene->addItem(bullet2);
                 bullet2->setPos(spaceship->x()+66,spaceship->y());
